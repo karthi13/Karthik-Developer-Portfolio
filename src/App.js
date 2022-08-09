ecd9caee5b1422b6254394398279d383333b64bd
Karthikeyan <dkarthikeyanmailbox@gmail.com>
2022-08-09 22:08:28 +0200

    First skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+
 function App() {
   return (
     <div>
-      <header class="text-3xl font-bold underline">
-          Learn React
-      </header>
+      <NavBar/>
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
     </div>
   );
 }
